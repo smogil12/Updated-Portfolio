91a928d8ddd02d3c31b3e7bcfe1cab090d06061d
Spencer Mogil <smogil12@gmail.com>
2020-01-12 00:30:24 -0500

    feat: click on text to generate link. still have to finish

---
+++ contact.js
@@ -0,0 +1,20 @@
+$(document).ready(function() {
+  $(".linkedin").on("click", function() {
+    var a = document.createElement("a");
+
+    // Create the text node for anchor element.
+    var link = document.createTextNode("This is link");
+
+    // Append the text node to anchor element.
+    a.appendChild(link);
+
+    // Set the title.
+    a.title = "This is Link";
+
+    // Set the href property.
+    a.href = "https://www.geeksforgeeks.org";
+
+    // Append the anchor element to the body.
+    $(".linkedin").html(a);
+  });
+});
