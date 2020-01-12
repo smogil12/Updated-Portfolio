360702d23032055c45a57f3e4886299b519d9ae4
Spencer Mogil <smogil12@gmail.com>
2020-01-12 12:54:40 -0500

    feat: email me by clicking click for email

--- contact.js
+++ contact.js
@@ -2,19 +2,27 @@ $(document).ready(function() {
   $(".linkedin").on("click", function() {
     var a = document.createElement("a");
 
-    // Create the text node for anchor element.
     var link = document.createTextNode("This is link");
 
-    // Append the text node to anchor element.
     a.appendChild(link);
 
-    // Set the title.
     a.title = "This is Link";
 
-    // Set the href property.
     a.href = "https://www.linkedin.com/in/spencer-mogil/";
 
-    // Append the anchor element to the body.
     $(".linkedin").html(a);
+  });
+  $(".email").on("click", function() {
+    var a = document.createElement("a");
+
+    var link = document.createTextNode("smogil12@gmail.com");
+
+    a.appendChild(link);
+
+    a.title = "This is Link";
+
+    window.location.href = "mailto:smogil12@gmail.com";
+
+    $(".email").html(a);
   });
 });
